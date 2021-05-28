3a656f7100357fce13d92ef3f0c1b16d4ceb2326
Christopher Campbell <campbefs@gmail.com>
2021-05-28 01:30:18 -0700

    front end up and running with handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Question, User, Score, } = require('../models');
+// const withAuth = require('../utils/auth');
+
+// Login
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// BOX OFFICE "/" - HOMEPAGE
+router.get('/', (req, res) => {
+  res.render('box-office')
+})
+
+// QUIZ
+router.get('/quiz', (req, res) => {
+  res.render('quiz')
+})
+
+// HIGH SCORES
+router.get('/highscores', (req, res) => {
+  res.render('highscores')
+})
+
+module.exports = router;
+
+
+
+
+
